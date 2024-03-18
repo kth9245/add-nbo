b966ee90186713bed8c8b4ea169eb7ab57873409
kth9245 <kth9245@korea.ac.kr>
2024-03-19 00:24:44 +0900

    Add add-nbo.c

--- add-nbo.c
+++ add-nbo.c
@@ -1,9 +1,23 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 
 uint32_t fton(char *file)
 {
 	FILE *fp = fopen(file, "rb");
+	if (fp == NULL) {
+		printf("Can't open file: %s\n", file);
+		return 0;
+	}
+	
+	fseek(fp, 0, SEEK_END);
+	long int size = ftell(fp);
+	if (size < sizeof(uint32_t)) {
+		printf("%s has too small size\n", file);
+		return 0;
+	}
+	fseek(fp, 0, SEEK_SET);
+	
 	uint32_t n;
 	fread(&n, sizeof(uint32_t), 1, fp);
 	fclose(fp);
@@ -25,5 +39,11 @@ int main(int argc, char *argv[]){
 	uint32_t n1 = fton(f1);
 	uint32_t n2 = fton(f2);
 	
+	if (n1*n2 == 0){
+		printf("Something Error\n");
+		return 1;
+	}
 	printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", n1, n1, n2, n2, n1+n2, n1+n2);
+	return 0;
 }
+
